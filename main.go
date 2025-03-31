bc66392f7d1f26b27a32bd2f9c007b8d4adf2189
Masaki Hara <ackie.h.gmai@gmail.com>
2025-03-31 11:25:30 +0900

    Bump echo from v3 to v4

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/ghodss/yaml"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/nlopes/slack"
 )
 
